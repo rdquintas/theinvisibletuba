ce591327bd36c75a214cb7345c6fe7c0cf40b2c4
Ricardo Quintas <rqcenter-web@yahoo.com>
2023-08-30 17:35:41 +0100

    php test

--- test/index.php
+++ test/index.php
@@ -1,6 +1,27 @@
 <?php 
 
-phpinfo();
+
+// inclusão da framework no código
+require_once '../lib/swift_required.php';
+
+// definir a autenticação via SMTP
+// mail.dominios.pt -> deverá trocar pelo endereço de e-mail do seu domínio
+// webmaster@dominio.tld -> deverá trocar pelo seu endereço de e-mail
+// password_caixa_email -> deverá preencher com a password da respectiva caixa
+$transport = Swift_SmtpTransport::newInstance('mail.theinvisibletuba.com', 25)
+->setUsername('info@theinvisibletuba.com')
+->setPassword('#uUcpSANQqA5')
+;
+$mailer = Swift_Mailer::newInstance($transport);
+ 
+// Criar o cabeçalho, assim como a mensagem
+$message = Swift_Message::newInstance('Assunto email')
+->setFrom(array('info@theinvisibletuba.com' => 'webmaster'))
+->setTo(array('rdquintas@yahoo.com'))
+->setBody('Corpo da mensagem')
+;
+// Efectuar o envio
+$result = $mailer->send($message);
 
 // // if (isset($_POST['submit'])) {
 //     $to = "rdquintas@yahoo.com";
