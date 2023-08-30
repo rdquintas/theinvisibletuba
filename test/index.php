0c3aeaa9c9f5737b1cb748a19c24c2bf8d5d24d4
Ricardo Quintas <rqcenter-web@yahoo.com>
2023-08-30 17:01:00 +0100

    php test

--- test/index.php
+++ test/index.php
@@ -1,2 +1,19 @@
-<?php
-echo 'PHP version: ' . phpversion();
+<?php 
+
+// if (isset($_POST['submit'])) {
+    $to = "rdquintas@yahoo.com";
+    //$_POST['email']; 
+    $subject = "zrqSubject";
+    $message = "zrqMessage"; //getRequestURI();
+    $from = "rdquintas@yahoo.com";
+    $headers = "From:" . $from;
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Mail Sent.";
+    }
+    else {
+        echo "failed";
+    }
+// }
+
+?>
