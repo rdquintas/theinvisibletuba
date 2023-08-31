9c7012b61226d0ca4a6de4af4c029f0d10b19814
Ricardo Quintas <rqcenter-web@yahoo.com>
2023-08-31 19:09:03 +0100

    php test

---
+++ forms/assets/scripts/zrq.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $("#typeEventCorporate").change(function () {
+    var newOptions = {
+        "Choose one": "Choose one",
+        "Office Party": "Office Party",
+        "Christmas Party": "Christmas Party",
+        "Store Opening": "Store Opening",
+        "Product Promo": "Product Promo",
+        "Festival": "Festival",
+        "Fair/Conference": "Fair/Conference",
+        "Other": "Other",
+      };
+
+      var $el = $("#occasion");
+      $el.empty();
+      $.each(newOptions, function (key, value) {
+        $el.append($("<option></option>").attr("value", value).text(key));
+      });
+  });
+
+  $("#typeEventPrivate").change(function (oEvvbet) {
+    var newOptions = {
+        "Choose one": "Choose one",
+        "Wedding": "Wedding",
+        "Student Event": "Student Event",
+        "Birthday Event": "Birthday Event",
+        "Other": "Other",
+      };
+  
+    var $el = $("#occasion");
+    $el.empty();
+    $.each(newOptions, function (key, value) {
+      $el.append($("<option></option>").attr("value", value).text(key));
+    });
+
+  });
+});
