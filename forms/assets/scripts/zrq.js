56bb098853b2a10263488f8a5bbb29f0ff367286
Ricardo Quintas <rqcenter-web@yahoo.com>
2023-09-01 10:42:07 +0100

    php test

--- forms/assets/scripts/zrq.js
+++ forms/assets/scripts/zrq.js
@@ -43,3 +43,39 @@ $(document).ready(function () {
     this.classList.add("was-validated");
   });
 });
+
+
+$(function () {
+  // use plugins and options as needed, for options, detail see
+  // https://www.i18next.com
+  i18next
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(i18nextBrowserLanguageDetector)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      debug: true,
+      fallbackLng: 'en',
+      resources: {
+        en: {
+            translation: {
+              intro: {
+                title: 'Landing Page zrq',
+                subTitle: 'Some subtitle'
+              }
+            }
+          }
+      }
+    }, (err, t) => {
+      if (err) return console.error(err);
+
+      // for options see
+      // https://github.com/i18next/jquery-i18next#initialize-the-plugin
+      jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+
+      // start localizing, details:
+      // https://github.com/i18next/jquery-i18next#usage-of-selector-function
+      $('body').localize();
+    });
+});
