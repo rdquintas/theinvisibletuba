ce94793d6a662c62feab537ada5eaeb7b39cb462
Ricardo Quintas <rqcenter-web@yahoo.com>
2025-01-31 16:01:50 +0000

    updating CORPORATE

--- landing/corporate/send.php
+++ landing/corporate/send.php
@@ -1,11 +1,11 @@
 <?php
-// require_once '../../lib/swift_required.php';
+require_once '../../lib/swift_required.php';
 
-// $transport = Swift_SmtpTransport::newInstance('mail.theinvisibletuba.com', 25)
-// ->setUsername('info@theinvisibletuba.com')
-// ->setPassword('yyyy');
+$transport = Swift_SmtpTransport::newInstance('mail.theinvisibletuba.com', 25)
+    ->setUsername('info@theinvisibletuba.com')
+    ->setPassword('yyyy');
 
-// $mailer = Swift_Mailer::newInstance($transport);
+$mailer = Swift_Mailer::newInstance($transport);
 
 $form = new stdClass();
 
@@ -24,50 +24,43 @@ if ($form->newsletter_cb == "true") {
     $form->newsletter_cb = "não";
 }
 
-// print_r($form);
-// echo($form);
+$body = prepareBody($form);
 
-header('Content-Type:application/json; charset=UTF-8');
-echo json_encode($form);
+$messageForTuba = createMessage($form->email, 'info@theinvisibletuba.com', $subjectForTuba, $body);
+$messageForCustomer = createMessage('info@theinvisibletuba.com', $form->email, $subjectConfirmation, $body);
 
-// $body = prepareBody($form);
+if ($mailer->send($messageForTuba) and $mailer->send($messageForCustomer)) {
+    //header('Location: thankyou.html');
+    header('Content-Type:application/json; charset=UTF-8');
+    echo json_encode($form);
+    exit();
+}
 
-// echo ($body);
-
-// $messageForTuba = createMessage($form->email, 'info@theinvisibletuba.com', $subjectForTuba, $body);
-// $messageForCustomer = createMessage('info@theinvisibletuba.com', $form->email, $subjectConfirmation, $body);
-
-// if ($mailer->send($messageForTuba) and $mailer->send($messageForCustomer)) {
-//     header('Location: thankyou.html');
-//     exit();
-// }
-
-// function createMessage($from, $to, $subject, $body)
-// {
-//     $message = Swift_Message::newInstance($subject)
-//         ->setFrom(array($from))
-//         ->setTo(array($to))
-//         ->setBody($body);
-
-//     return $message;
-// }
+function createMessage($from, $to, $subject, $body)
+{
+    $message = Swift_Message::newInstance($subject)
+        ->setFrom(array($from))
+        ->setTo(array($to))
+        ->setBody($body);
+    return $message;
+}
 
 
-function prepareBody($form) {
+function prepareBody($form)
+{
     $body = "";
-    $body .= "Olá " . $form->name . ",\r\n";
-    $body .= "Obrigado pelo teu pedido, entraremos em contacto contigo o mais rápido possível e enviaremos uma proposta de orçamento para o teu email." . "\r\n\r\n";
-    $body .= "Sumário do pedido efectuado:" . "\r\n";
+    $body .= "Olá " . $form->nome . ",\r\n";
+    $body .= "Obrigado pela tua mensagem, entraremos em contacto contigo o mais rápido possível." . "\r\n\r\n";
+    $body .= "Sumário da mensagem recebida:" . "\r\n";
     $body .= "=============" . "\r\n";
-    $body .= "NAME: " . $form->nome . "\r\n";
+    $body .= "NOME: " . $form->nome . "\r\n";
     $body .= "EMAIL: " . $form->email . "\r\n";
     $body .= "EMPRESA: " . $form->empresa . "\r\n";
     $body .= "MENSAGEM: " . $form->mensagem . "\r\n";
     $body .= "RECEBER NEWSLETTER: " . $form->newsletter_cb . "\r\n";
     $body .= "=============" . "\r\n";
     $body .= "\r\n\r\n" . "Muito Obrigado ;o)" . "\r\n\r\n";
-    $body .= "/The Invisible Tuba" . "\r\n\r\n\r\n\r\n\r\n";;
+    $body .= "/The Invisible Tuba" . "\r\n";
+    $body .= "Ricardo Quintas | T: 967 285 043";
     return $body;
 }
-
-?>
