6fbd76dffdef8356baa47192b3ed93f3c0e86a73
Ricardo Quintas <rqcenter-web@yahoo.com>
2025-10-28 13:58:44 +0000

    fix corporate

--- assets copy/scripts/window-settings.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var globalWindowWidth = $(window).width();
-
-// ==========================================================================
-// Global Resize
-// ==========================================================================
-$(window).on('resize', function() {
-  globalWindowWidth = $(window).width();
-});
